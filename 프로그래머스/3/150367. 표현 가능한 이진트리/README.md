af25ddc9643e62001ed4632c1fe859357c6de77d
남지안 <59587009+Jian-Nam@users.noreply.github.com>
2024-04-10 19:10:50 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 69.08 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/README.md
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/README.md
@@ -1,6 +1,6 @@
 # [level 3] 표현 가능한 이진트리 - 150367 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 19:10:41
+2024년 04월 10일 19:10:49
 
 ### 문제 설명
 
--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,43 +1,13 @@
 def solution(numbers):
-    dp = {}
-    dp[1] = 1
-    dp[0] = 1
-    def get_binary(num):
-        binary = []
-
-        while(num > 0):
-            binary.append(num % 2)
+    def get_expon(num):
+        expon = 1
+        while(num > 1):
             num //= 2
-        
-        full_len = 1
-        while(len(binary) > full_len - 1):
-            full_len *= 2
-        
-        while(len(binary) < full_len - 1):
-            binary.append(0)
-            
-        return binary
-    
-    def split_binary(binary):
-
-        stack = []
-        stack.append(binary)
-        while(stack):
-            b = stack.pop()
-            # print(binary, b)
-            if(sum(b) == 0):
-                continue
-            mid = len(b) // 2
-            if(b[mid] == 1):
-                stack.append(b[:mid])
-                stack.append(b[mid+1:])
-            else:
-                return 0
-        return 1
-        
-    answer = []
+            expon += 1
+        return expon
     for n in numbers:
-        binary = get_binary(n)
-        answer.append(split_binary(binary))
-    
+        expon1 = get_expon(n)
+        expon2 = get_expon(expon1)
+        print(expon1, expon2)
+    answer = []
     return answer
