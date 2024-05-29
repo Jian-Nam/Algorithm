69030c717f7a2e767eedbcfa207418827da78b1f
남지안 <59587009+Jian-Nam@users.noreply.github.com>
2024-05-29 20:16:21 +0900

    [level 3] Title: 거스름돈, Time: 355.58 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,10 @@
+def solution(n, money):
+    dp = [0 for _ in range(n+1)]
+    
+    for m in money:
+        dp[m] += 1
+        for i in range(m, n+1):
+            dp[i] += dp[i - m]
+        
+
+    return dp[n]
